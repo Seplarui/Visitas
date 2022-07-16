5f4a9182daf3064a1677bc9517fb60c74b2ad48d
Sebas <seplarui@gmail.com>
2022-07-17 00:43:21 +0200

    first commit

---
+++ src/test/java/com/calendario/visitas/VisitasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calendario.visitas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisitasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
